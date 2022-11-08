0bce79df43b2154766534a655051363c80c70b25
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-11-08 15:09:10 +0100

    add random mails

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,21 @@
-alert('ciao')
+const { createApp } = Vue
+
+createApp({
+    data() {
+      return {
+        mails: []
+      }
+    },
+    mounted() {
+      for(let i = 0; i < 10; i++){
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response =>{
+          // console.log(response.data.response);
+          this.mails.push(response.data.response);
+          // console.log(this.mails)
+        })
+      }
+    }
+  }).mount('#app')
+
